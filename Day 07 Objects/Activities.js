53061106bc3f70ad865e80ab995cce40025b06e8
jatin <jatinahirwar07@gmail.com>
2024-07-22 01:05:00 +0530

    Day 7: objects

---
+++ Day 07 Objects/Activities.js
@@ -0,0 +1,25 @@
+// activity 1 : object creation and access
+// task 1
+const booksDetails = {
+    book1: {
+        name: "The Adventures of Huckleberry Finn",
+        author: "Mark Twain",
+        year: 1884
+    },
+    book2: {
+        name: "War and Peace",
+        author: "Lev Tolstoy",
+        year: 1869
+    },
+    book3: {
+        name: "Jane Eyre",
+        author: "Charlotte Brontë ",
+        year: 1847
+    },
+}
+
+console.log(booksDetails);
+
+// task 2
+
+console.log();
